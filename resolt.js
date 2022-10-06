6549486de506be6e3cfdcc09b0c487db493a06c4
jkesh <jkeshi@gmail.com>
2022-10-06 17:51:07 -0400

    changes to my project

---
+++ resolt.js
@@ -0,0 +1,29 @@
+"use script"
+
+function estumateBtnClicked() {
+    const checkInDate = new Date(Document.getElementById("checkInDate").value);
+     const numberOfNight = Document.getElementById("numberOfNights").value;
+}
+
+Let priceForQk = 150 * numberOfNights;
+let priceOfSuite = 210 * numberOfNights;
+let roomType = "king"
+let currentMonth = checkInDate.getMonth();
+let inSeaon = false
+switch (currentMonth) {
+    case value5:
+    case 6:
+    case 7:
+        inSeason = tue;
+        break;
+        default;
+        inSeason = false;
+        break;
+}
+
+if (inSeason && roomType) {
+let priceForQk = 250 * numberOfNights
+let priceOfSuite = 250 * numberOfNights
+} else {
+    
+}
